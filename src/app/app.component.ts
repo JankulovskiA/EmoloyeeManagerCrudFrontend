040b19610370bbdec738a188ef19c05834f118d2
aleks <aleksandar.j516@gmail.com>
2023-09-19 23:56:16 +0200

    initial service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'EmployeeManagerFronend';
+export class AppComponent implements OnInit {
+  public employees: Employee[] = [];
+  
+  constructor (private employeeSerice: EmployeeService){}
+  ngOnInit(): void {
+    this.getEmployees()
+  }
+
+  public getEmployees(): void {
+    this.employeeSerice.getEmployees().subscribe(
+      (response: Employee[]) => {
+      this.employees = response;
+    },
+    (error: HttpErrorResponse) => {
+      alert(error.message); 
+    });
+  }
 }
